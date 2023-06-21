48a26222496c219d1f8571180ae4f9bd6025366b
medinalicina <medinalicina94@gmail.com>
2023-06-21 11:04:47 +0200

    kalkulator

---
+++ dom.2kalkulator/app.js
@@ -0,0 +1,4 @@
+//queryselector all on selektuje sve sto postoji a naprimer bez ovog all on selektuje i prikazuje samo prvi
+//for each prolazi kroz sve (ceo kod) slican je mapu
+//add event listener on slusa svaku promenu
+//e.target odradjuje te neke primene e je event

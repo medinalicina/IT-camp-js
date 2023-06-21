c8847932144852d4ad7c9c228e051f180e438fa7
medinalicina <medinalicina94@gmail.com>
2023-06-21 21:57:28 +0200

    kalkulator

--- dom.2kalkulator/app.js
+++ dom.2kalkulator/app.js
@@ -2,3 +2,4 @@
 //for each prolazi kroz sve (ceo kod) slican je mapu
 //add event listener on slusa svaku promenu
 //e.target odradjuje te neke primene e je event
+//da se uradi kalkulator pei cemu cemo brojeve ispisivati u gornjnem delu kalkulatora,  dodati takodje i puta, podeljeno, i sve te funkcije da se odradjuu i prikazuju u konzolama

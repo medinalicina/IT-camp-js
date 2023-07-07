6632d58df4c5d426dc53aa29a2f0a8b53a48c8f2
medinalicina <medinalicina94@gmail.com>
2023-07-07 11:17:54 +0200

    fetch

--- ash.java3/app.js
+++ ash.java3/app.js
@@ -1,9 +1,17 @@
 //clousure hoisting ero function promises
-fetch('https://dummyjson.com/products')
+// fetch('https://dummyjson.com/products')
+// .then(res => res.json())
+// .then((json) => {
+//     console.log(json.products);
+//     return json.products.filter((el)=> el.stock >= 68);
+// })
+// .then((data) => console.log(data));
+    
+fetch('https://dummyjson.com/carts')
 .then(res => res.json())
 .then((json) => {
-    console.log(json.products);
-    return json.products.filter((el)=> el.stock >= 68);
-})
-.then((data) => console.log(data));
-    
+         console.log(json.products);
+         return json.carts.map((el)=> el.products.filter((nesto) => nesto.total>= 555));
+     })
+
+    .then((data) => console.log(data));
